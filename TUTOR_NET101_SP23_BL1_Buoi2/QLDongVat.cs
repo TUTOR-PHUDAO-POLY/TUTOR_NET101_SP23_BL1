fb2130997188a8bac33d2f19c918410cbc444b17
NLHGiang <nlhg090602@gmail.com>
2023-02-14 12:55:35 +0700

    GIANGNLH_B2

---
+++ TUTOR_NET101_SP23_BL1_Buoi2/QLDongVat.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTOR_NET101_SP23_BL1_Buoi2
+{
+    internal class QLDongVat
+    {
+        List<DongVat> listDongVat = new List<DongVat>()
+        {
+            new DongVat("1","A",1,1,1),
+            new DongVat("1","W",1,1,2),
+            new DongVat("1","G",1,1,5),
+            new DongVat("4","B",1,1,1),
+            new DongVat("5","C",1,1,3),
+            new DongVat("6","D",1,1,7),
+        };
+
+        public void HienThi()
+        {
+            foreach (var dv in listDongVat)
+            {
+                dv.Display();
+            }
+        }
+
+        public void SapXepTangDanTheoTen()
+        {
+            listDongVat = (from dv in listDongVat
+                           orderby dv.Ten
+                           select dv).ToList();
+        }
+
+        public void SapXepGiamDanTheoCanNang()
+        {
+            listDongVat = (from dv in listDongVat
+                           orderby dv.CanNang descending
+                           select dv).ToList();
+        }
+
+        public void TimKiemTheoTen(string tenCanTim)
+        {
+            Console.WriteLine("Dong vat can tim");
+            foreach (var dv in listDongVat)
+            {
+                // string.equals
+                if (dv.Ten == tenCanTim)
+                {
+                    dv.Display();
+                }
+            }
+        }
+
+        public void XoaTheoMa(string maCanXoa)
+        {
+            // for có thể xóa nhiều hơn 1 phần tử
+            //for (int i = 0; i < listDongVat.Count(); )
+            //{
+            //    // Khi xuất hiện phần tử để xóa => i giữ nguyên giá trị, không xuất hiện => i++ để kiểm tra phần tử tiếp theo
+            //    if (listDongVat[i].Ma == maCanXoa)
+            //    {
+            //        listDongVat.Remove(listDongVat[i]);
+            //    }
+            //    else
+            //    {
+            //        i++;
+            //    }    
+            //}
+
+            // foreach khi xóa nhiều nhất 1 phần tử
+            //foreach (var dv in listDongVat)
+            //{
+            //    // string.equals
+            //    if (dv.Ma == maCanXoa)
+            //    {
+            //        listDongVat.Remove(dv);
+            //        break; // lưu ý có break
+            //    }
+            //}
+        }
+    }
+}
---
+++ TUTOR_NET101_SP23_BL1_Buoi2/SinhVien.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUTOR_NET101_SP23_BL1_Buoi2
+{
+    internal class SinhVien
+    {
+        string maSv;
+        string hoTen;
+        int namSinh;
+        string chuyenNganh;
+        double diemTB;
+
+        // ctor
+        public SinhVien()
+        {
+            //maSv = "PH000";
+        }
+
+        public SinhVien(string maSv, string hoTen, int namSinh, string chuyenNganh, double diemTB)
+        {
+            this.maSv = maSv;
+            this.hoTen = hoTen;
+            this.namSinh = namSinh;
+            this.chuyenNganh = chuyenNganh;
+            this.diemTB = diemTB;
+        }
+
+        public string MaSV { get { return maSv; } set { maSv = value; } }
+        public string HoTen { get { return hoTen; } set { hoTen = value; } }
+        public int NamSinh { get { return namSinh; } set { namSinh = value; } }
+        public string ChuyenNganh { get { return chuyenNganh; } set { chuyenNganh = value; } }
+        public double DiemTB { get { return diemTB; } set { diemTB = value; } }
+
+        public void Display()
+        {
+            Console.WriteLine($"{MaSV} - {HoTen} - {NamSinh} - {ChuyenNganh} - {DiemTB}");
+        }
+    }
+}
